cbe8f9f7a6b75fef21377b085279eba1ffbabdbf
ravi-kolla <ravitejakolla29@gmail.com>
2020-08-22 02:28:34 +0530

    wishes route style fixes

--- pages/wishes/[uid].js
+++ pages/wishes/[uid].js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Col, Breadcrumb } from 'react-bootstrap'
+import { Container, Col } from 'react-bootstrap'
 import { Image, RichText, Date } from 'prismic-reactjs'
 import { client } from '../../prismic-configuration'
 import Link from 'next/link'
@@ -8,21 +8,17 @@ import Layout from '../../components/Layout'
 const Wishes = props =>{
   return (
     <Layout>
-    <Container className="mt-5">
-      <Breadcrumb>
-        <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-        <Breadcrumb.Item active>{RichText.render(props.wishes.data.title)}</Breadcrumb.Item>
-      </Breadcrumb>
-      <Col className="mx-auto" md={8}>
+    <div className="mt-5">
+      <Col>
       {RichText.render(props.wishes.data.title)}
       <div className="mt-2 mb-3">
-      <img src={props.wishes.data.image.url} alt={props.wishes.data.image.alt} />
+      <img className="mx-auto" src={props.wishes.data.image.url} alt={props.wishes.data.image.alt} />
       </div>
       <div>
       {RichText.render(props.wishes.data.body)}
       </div>
       </Col>
-    </Container>
+    </div>
     </Layout>
   )
 }
